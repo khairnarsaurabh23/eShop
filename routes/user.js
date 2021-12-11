8d14596fa9f4725e25b61371a99ebd643e343a7b
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-12-12 00:27:41 +0530

    eshop: Online Shoping Store

--- routes/user.js
+++ routes/user.js
@@ -25,41 +25,6 @@ const {signup,
 //importing middlewares
 const {userMiddleware, userRole} = require('../middlewares/user');
 
-
-// //middleware for restricting routes to logged in user
-// const userMiddleware = app.use(async function(req, res, next){
-//   // check token first in cookies
-//   let token = req.cookies.token;
-//
-//   // if token not found in cookies, check if header contains Auth field
-//   if (!token && req.header("Authorization")) {
-//     token = req.header("Authorization").replace("Bearer ", "");
-//   }
-//
-//   if (!token) {
-//     return next(new customError("Login first to access this page", 401));
-//   }
-//
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//
-//   req.user = await User.findById(decoded.id);
-//
-//   next();
-// });
-// //middleware for checking the role of user
-// const userRole = (...role) => {
-//     //by spreading role array parameter sent by route will
-//     //auto added to the role array
-//     return (req, res, next) => {
-//         //check if user had specific role
-//         //typo in user model :roll
-//         if (!role.includes(req.user.roll)) {
-//             return next(new customError('You are not permited to perform this activity', 403));
-//         }
-//         next();
-//     };
-// };
-
 router.route('/signup').post(signup);
 router.route('/login').get(login);
 router.route('/logout').get(logout);
